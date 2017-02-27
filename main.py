97de39ade3b02b3a9b413470469234aa37bc2c2c
Javier Herrera <muchorrollodejavi@hotmail.com>
2017-02-27 11:16:31 -0500

    Added integration with async telepot

--- main.py
+++ main.py
@@ -1,8 +1,40 @@
 import cyclos_api
+import telepot
+import asyncio
+from telepot.aio.delegate import (pave_event_space, per_chat_id,
+                                  create_open)
+
 import config
 
 
+class BotHandler(telepot.aio.helper.ChatHandler):
+    def __init__(self, *args, **kwargs):
+        super(BotHandler, self).__init__(*args, **kwargs)
+
+    async def on_chat_message(self, msg):
+        content_type, chat_type, chat_id = telepot.glance(msg)
+        await self.sender.sendMessage('hola ' + str(chat_id))
+
+        data = cyclos_api.get_account_balance(config.NAME,
+                                              config.PASSWORD)
+
+        await self.sender.sendMessage('Saldo: ' + data['balance'] +
+                                      '\nCrédito disponible: ' +
+                                      data['availableBalance'])
+
+        print('saldo: ' + data['balance'])
+        print('\n Crédito disponible: ' + data['availableBalance'])
+
+
 if __name__ == "__main__":
-    data = cyclos_api.get_account_balance(config.NAME, config.PASSWORD)
-    print('saldo: ' + data['balance'])
-    print('\n Crédito disponible: ' + data['availableBalance'])
+
+    bot = telepot.aio.DelegatorBot(config.TOKEN, [
+        pave_event_space()(
+            per_chat_id(), create_open, BotHandler, timeout=100),
+        ])
+
+    loop = asyncio.get_event_loop()
+    loop.create_task(bot.message_loop())
+    print('Listening')
+
+    loop.run_forever()

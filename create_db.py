58a2394df941df1806be0b3b84b5261f8dab270d
Javier Herrera <muchorrollodejavi@hotmail.com>
2017-03-01 21:52:47 -0500

    finished script to create the db structure

--- create_db.py
+++ create_db.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Integer, String, MetaData, create_engine, Table, Column
 
-# from config import DB_NAME, DB_USER, DB_PASSWORD, DB_HOST
+from config import DB_NAME, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT
 
 
 metadata = MetaData()
@@ -8,11 +8,17 @@ metadata = MetaData()
 # Creamos la estructura de la tabla
 
 users = Table('users', metadata,
-                 Column('chat_id', Integer, primary_key=True),
-                 Column('username', String),
-                 Column('password', String),
-                 Column('token', String),
-                 )
+              Column('chat_id', Integer, primary_key=True),
+              Column('username', String),
+              Column('password', String),
+              Column('token', String),
+              )
 
-engine = create_engine('sqlite:///:memory:', echo=True)
+engine = create_engine('postgresql://' +
+                       DB_USER + ':' +
+                       DB_PASSWORD + '@' +
+                       DB_HOST + ':' +
+                       DB_PORT + '/' +
+                       DB_NAME
+                       )
 metadata.create_all(engine)
